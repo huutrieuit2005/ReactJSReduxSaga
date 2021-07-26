a24c35930d65ba8557b3f6747ad922793577fb15
huu trieu <huutrieuit2005@outlook.com>
2021-07-26 22:29:02 +0700

    create app bai 4

---
+++ my-app/src/app/rootSaga.ts
@@ -0,0 +1,12 @@
+import counterSaga from "features/counterSaga";
+import {all} from "redux-saga/effects";
+
+function* heloSaga(){
+    console.log("hello saga");
+    
+}
+
+export default function* rootSaga() {
+  console.log('rootSaga log');
+  yield all([heloSaga(), counterSaga()],);
+}
